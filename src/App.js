c4bb8e29ded0047a176812ee9e0e7b77a1a23ab9
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-27 22:31:11 +0600

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
 import Topbar from './components/Topbar/Topbar';
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">Others</div>
+      </div>
     </div>
   );
 }
