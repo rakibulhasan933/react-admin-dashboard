fad16cdf4a9f5b3ca6a78eaca67ed6d3f3552049
rakibulhasan933 <rakibulmd933@gmail.com>
2022-05-02 11:27:59 +0600

    new user components add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import TopBar from './components/TopBar/TopBar';
+import AddUser from './pages/AddUser/AddUser';
 import Home from './pages/Home/Home';
 import HomeMain from './pages/HomeMain/HomeMain';
 import UserList from './pages/UserList/UserList';
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<HomeMain />}>
           <Route index element={<Home />} />
           <Route path='/user' element={<UserList />} />
+          <Route path='/newUser' element={<AddUser />} />
         </Route>
       </Routes>
     </div>
---
+++ src/pages/AddUser/AddUser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddUser.css';
+
+const AddUser = () => {
+    return (
+        <div>
+            <h1>Add user</h1>
+        </div>
+    );
+};
+
+export default AddUser;
